940a7bb8bb812eaf5ecfaa1edb54b5f1cf9a1b71
Adarsh Ranjan <adarshranjan4242@gmail.com>
2022-06-25 16:25:26 +0530

    All files are updated

---
+++ index.js
@@ -0,0 +1,80 @@
+var API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+//const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const next = document.getElementById('next');
+const back = document.getElementById('back');
+const backp = document.querySelector('.back-page');
+
+var i=1;
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    } else if(vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+next.addEventListener('click',()=>{
+    i++;
+    console.log(i);
+    API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page='+ `${i}`;
+    getMovies(API_URL)
+    console.log(API_URL);
+
+
+})
+
+back.addEventListener('click',()=>{
+    if(i>1)
+    {
+        
+        i--;
+        console.log(i);
+        API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page='+ `${i}`;
+        getMovies(API_URL)
+        console.log(API_URL);
+
+    }
+})
+
+
